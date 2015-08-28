1ba56ceffd5a8d3ac53206fa2339088837c01256
Jens Andre Kålen <ja.kaalen@gmail.com>
2015-08-28 12:49:39 +0200

    Prettified and fixed editing of program

---
+++ src/LiftBro.Tests/ProgramTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LiftBro.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        public void New_program_is_created_when_saved_by_non_owner()
+        {
+            
+        }
+    }
+}
