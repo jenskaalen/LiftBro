4cd0752e63273b528d91e33dc3858c2b34bdcefc
Jens Andre Kålen <ja.kaalen@gmail.com>
2015-08-31 14:24:06 +0200

    renamed routine to profile

--- src/LiftBro.Web/Scripts/app/profile.js
+++ src/LiftBro.Web/Scripts/app/profile.js
@@ -1,4 +1,4 @@
-﻿angular.module('main').controller('routineController', function($scope, $http) {
+﻿angular.module('main').controller('profileController', function($scope, $http) {
     $scope.lastSetWorkout = 0;
     var currentWorkoutDay = 0;
     var currentExercise = 0;
@@ -31,14 +31,6 @@
     $http.get('/api/WorkoutDay/GetCompletedWorkouts?take=10&skip=0').success(function(completedWorkouts) {
         $scope.completedWorkouts = completedWorkouts;
     });
-    //$http.get('/api/Program/GetUserPrograms').success(function (programs) {
-    //    $scope.programs = programs;
-    //    $scope.currentWorkout = programs[0].workoutDays[currentWorkoutDay];
-    //    $scope.currentProgram = programs[0];
-
-    //    $scope.currentExercise = programs[0].workoutDays[currentWorkoutDay].exercises[0];
-    //    console.log($scope.currentExercise);
-    //});
 
     $http.get('/api/Program/GetUserExercises').success(function (exercises) {
         $scope.exercises = new Array();
