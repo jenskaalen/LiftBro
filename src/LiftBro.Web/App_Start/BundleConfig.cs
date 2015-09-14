1db24f246e4a99bba00b42e29ccfde912746ea08
Jens Andre Kålen <ja.kaalen@gmail.com>
2015-09-14 12:30:23 +0200

    removed bundle rendering of angular scripts

--- src/LiftBro.Web/App_Start/BundleConfig.cs
+++ src/LiftBro.Web/App_Start/BundleConfig.cs
@@ -25,16 +25,6 @@ namespace LiftBro.Web
                         "~/Scripts/angular-resource.js",
                         "~/Scripts/angular-route.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/app").Include(
-                        "~/Scripts/app/app.js",
-                        "~/Scripts/app/services.js",
-                        "~/Scripts/app/directives.js",
-                        "~/Scripts/app/main.js",
-                        "~/Scripts/app/contact.js",
-                        "~/Scripts/app/about.js",
-                        "~/Scripts/app/demo.js"
-                        ));
-
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
